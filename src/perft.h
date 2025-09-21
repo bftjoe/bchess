0964f59a35fc7d9eb52f87f9b976b364010fd6a9
Joseph Huang <josephshuang@gmail.com>
2025-09-21 01:00:33 -0400

    remove perftmp

--- src/perft.h
+++ src/perft.h
@@ -8,8 +8,5 @@ namespace bchess {
 template<bool Div> size_t perft(Position &pos, int depth);
 void perft(Position &pos, int depth);
 
-template<bool Div> size_t perftmp(Position &pos, int depth);
-void perftmp(Position &pos, int depth);
-
 } /* namespace bchess */
 
